82dd5b67e8ef1d090ab266a5228be8534e7a48a4
shiva-0230 <24331a05e8@mvgrce.edu.in>
2025-12-18 21:45:42 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,13 @@
+a=14
+print(a)
+print(type(a))
+b='B'                                                                                                                                                                  
+print(b)
+print(type(b))
+c=9.9
+print(c)
+print(type(c))
+d="siva"
+print(d)
+print(type(d))
+
